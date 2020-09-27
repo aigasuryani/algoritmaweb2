4d3b4e526e58e871294b16ef1237a0e69d8d0ef6
aigasuryani <71936200+aigasuryani@users.noreply.github.com>
2020-09-27 15:02:52 +0700

    Add files via upload

---
+++ pratikum_web2/variabel_biasa.php
@@ -0,0 +1,15 @@
+<?php
+$nama = "Aiga suryani";
+$nim = 2018804491;
+$kelas = "SI shift 2018";
+$Kampus = "STMIK Insan Pembangunan F";
+echo "Nama Mahasiswa : $nama";
+echo "<br>";
+echo "Nim Mahasiswa : $nim";
+echo "<br>";
+echo "Kelas : $kelas";
+echo "<br>";
+echo "Nama Kampus : $Kampus";
+echo "<br>";
+
+?>
